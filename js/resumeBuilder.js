87aa4f443a1a54bcfb90366a846236a67f4c21a9
Joy <joylol@users.noreply.github.com>
2015-07-17 18:05:20 -0500

    Remove hello world text and append formattedName and formattedRole.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var formattedName = HTMLheaderName.replace("%data%", "Joy Thomas");
+ var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+ $ ("#header").append(formattedName);
+ $ ("#header").append(formattedRole);
